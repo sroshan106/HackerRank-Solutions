6b9525ca13026127799bd0c74fd4512817feecb8
Roshan singh <singhroshan106@gmail.com>
2020-07-02 17:44:41 +0530

    Update Gridland Metro.py

--- Gridland Metro.py
+++ Gridland Metro.py
@@ -2,29 +2,25 @@
 def gridlandMetro(n, m, k, track):
     if k==0:
         return m*n
-    visited=[0]*n
-    arr = [0]*m
-    counter = 0
-    prev = track[0][0]
+    d={}
+    counter = m*n
     for i in range(k):
         r,c1,c2 = track[i]
-        if prev!=r:
-            visited[prev-1]=1    
-            counter+= arr.count(0)
-            arr=[0]*m
-        for p in range(c1-1,c2):
-            arr[p]=1
-        prev=r
-
-
-    visited[r-1]=1   
-    counter+= arr.count(0)
-    temp = visited.count(0)
-    
-    counter+=temp*m
+        if r not in d:
+            d[r]=[c1,c2]
+        else:
+            left,right = d[r]
+            if left>c1:
+                left = min(left,c1)
+            if right<c2:
+                if c1>right:
+                    counter+=c1-right-1
+                right = max(right,c2)
+            d[r]=[left,right]
+    for i,j in d.values():
+        counter = counter - (j-i+1)
     return counter
 
-        
 nmk = input().split()
 
 n = int(nmk[0])
