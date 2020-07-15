a5bfc0e13ab6f66c38931bd2d4312040ca0b4c48
Roshan singh <singhroshan106@gmail.com>
2020-07-15 11:03:20 +0530

    Update Red Knight's Shortest Path.py

--- Red Knight's Shortest Path.py
+++ Red Knight's Shortest Path.py
@@ -5,26 +5,46 @@ import os
 import random
 import re
 import sys
-def isSafe(x,y):
+def isSafe(x,y,visited):
     global n
-    if x>=0 and x<n and y>=0 and y<n:
+    if x>=0 and x<n and y>=0 and y<n and visited[x][y]==[]:
         return True
     return False
 
 def printShortestPath(n, i_start, j_start, i_end, j_end):
+    d={0:'UL',1:'UR',2:'R',3:'LR',4:'LL',5:'L'}
     visited=[]
     for i in range(n):
-        visited.append([0]*n)
+        visited.append([[]]*n)
     queue=[]
-    moves=[[-2,-1],[2,1],[0,2],[2,1],[2,-1],[0,-2]]
-    visited[i_start][j_start]=1
-    queue.append([i_start,jstart])
+    moves=[[-2,-1],[-2,1],[0,2],[2,1],[2,-1],[0,-2]]
+    visited[i_start][j_start]=[0]
+    queue.append([0,i_start,j_start])
     while queue:
-        x,y = queue.pop()
-        for i in moves:
-            m,n = i
-            if isSafe(x+m,y+n)
+        counter,x,y = queue.pop(0)
+        if x==i_end and y==j_end:
+            return visited[x][y]
+        for i in range(6):
+            m,n = moves[i]
+            if isSafe(x+m,y+n,visited):
+                queue.append([counter+1,x+m,y+n])
+                if visited[x+m][y+n]!=[]:
+                    count= visited[x+m][y+n][0]
+                    if count>counter+1:
+                        visited[x+m][y+n]=visited[x][y].copy()
+                        visited[x+m][y+n][0]=counter+1
+                        visited[x+m][y+n].append(d[i])
+                    else:
+                        visited[x+m][y+n].append(d[i])
+                        visited[x+m][y+n][0]+=1
 
+                else:
+                    visited[x+m][y+n]=visited[x][y].copy()
+                    visited[x+m][y+n][0]+=1
+                    visited[x+m][y+n].append(d[i])
+        #print(queue)
+        #print('#',visited)
+    return -1
 
 
 n = int(input())
@@ -39,4 +59,10 @@ i_end = int(i_startJ_start[2])
 
 j_end = int(i_startJ_start[3])
 
-printShortestPath(n, i_start, j_start, i_end, j_end)
+temp = printShortestPath(n, i_start, j_start, i_end, j_end)
+if temp==-1:
+    print("Impossible")
+else:
+    print(temp[0])
+    for i in range(1,temp[0]+1):
+        print(temp[i],end=" ")
