2d4373c3f3ab288957e023c81e150e8b8c9ee635
Roshan singh <singhroshan106@gmail.com>
2020-07-16 17:09:30 +0530

    Update Kruskal (MST): Really Special Subtree.py

--- Kruskal (MST): Really Special Subtree.py
+++ Kruskal (MST): Really Special Subtree.py
@@ -1,26 +1,43 @@
-def third(elem):
-    return elem[2]
+import sys
+sys.setrecursionlimit(5000)
 
-def kruskals(g_nodes, g):
-    g = sorted(g,key = third)
-    visited=[0]*g_nodes
-    summ=0
+def third(elem):
+    small = elem[2]
+    summ = elem[0]+elem[1]+elem[2]
+    return (small,summ)
+
+
+def findParent(element ,parent):
+    if parent[element]==element:
+        return element
+    return findParent(parent[element],parent)
+
+
+def kruskals(n, edge):
+    edge = sorted(edge,key = third)
+    #print(edge)
+    
+    output =[0]*(n-1)
+
+    parent=[0]*n
+    for i in range(n):
+        parent[i]=i
     counter=0
-    for i in g:
-        to,From,weight = i
-        if to!=From:
-            if visited[to]!=1 or visited[From]!=1:
-                counter+=1
-                summ+=weight
-                if visited[to]==0:
-                    visited[to]=1
-                    
-                if visited[From]==0:
-                    visited[From]=1
-                    
-        if counter==g_nodes-1:
-            break
+    i=0
+    while counter!=n-1:
+        source,destination,weights = edge[i]
+        sourceParent = findParent(source,parent)
+        destinationParent =  findParent(destination,parent)
+        if  sourceParent!=destinationParent:
+            output[counter]=[source,destination,weights]
+            counter+=1
+            parent[source]=destinationParent
+        i+=1
+    summ=0
+    for i in output:
+       summ+=i[2]
     return summ
+    
 
 g_nodes, g_edges = map(int, input().rstrip().split())
 g=[]
@@ -29,9 +46,10 @@ for i in range(g_edges):
 
 for i in range(g_edges):
     g[i][0], g[i][1], g[i][2] = map(int, input().rstrip().split())
+    
     g[i][0]-=1
     g[i][1]-=1
-
+    
 res = kruskals(g_nodes, g)
 
 print(res)
