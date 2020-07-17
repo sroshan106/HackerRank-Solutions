a303c4bb2893060f400ebdea801b90e1ac160fbc
Roshan singh <singhroshan106@gmail.com>
2020-07-17 16:14:27 +0530

    Update Prim's (MST) : Special Subtree.py

--- Prim's (MST) : Special Subtree.py
+++ Prim's (MST) : Special Subtree.py
@@ -2,6 +2,7 @@
 import sys
 def minKey(key, mstSet,n): 
     minn = sys.maxsize
+    min_index=-1
     for v in range(n): 
         if key[v] < minn and mstSet[v] == False: 
             minn = key[v] 
@@ -9,24 +10,23 @@ def minKey(key, mstSet,n):
     return min_index 
 
 
-def prims(n, matrix):
+def prims(n, matrix,start):
     key= [sys.maxsize]*n
     #print(key)
     parent = [None]*n
-    key[0]=0
+    key[start]=0
     mstSet = [False]*n
-    parent[0]=-1
+    parent[start]=-1
 
     for i in range(n):
         u = minKey(key,mstSet,n)
-        mstSet[u]=True
-        print('%',u)
+        mstSet[u]=True  
         for v in range(n):
-            if matrix[u][v]>0 and mstSet==False and key[v]>matrix[u][v]:
+            if matrix[u][v]>-1 and mstSet[v]==False and key[v]>matrix[u][v]:
                 key[v]=matrix[u][v]
                 parent[v]=u
 
-    #print(parent)
+    return sum(key)
 
 
 nm = input().split()
@@ -39,13 +39,13 @@ m = int(nm[1])
 
 matrix=[]
 for i in range(n):
-    matrix.append([0]*n)
+    matrix.append([-1]*n)
 
 for _ in range(m):
     a,b,weight= (list(map(int, input().rstrip().split())))
     a-=1
     b-=1
-    if matrix[a][b]!=0:
+    if matrix[a][b]!=-1:
         matrix[a][b]=min(matrix[a][b],weight)
         matrix[b][a]=min(matrix[b][a],weight)
     else:
@@ -54,6 +54,6 @@ for _ in range(m):
 
 start = int(input())
 start-=1
-result = prims(n, matrix)
+result = prims(n, matrix,start)
 
 print(result)
