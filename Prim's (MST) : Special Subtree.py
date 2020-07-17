7bd0c5af6e896c2b5297e321720ae9706d9871fb
Roshan singh <singhroshan106@gmail.com>
2020-07-17 16:02:51 +0530

    Create Prim's (MST) : Special Subtree.py

---
+++ Prim's (MST) : Special Subtree.py
@@ -0,0 +1,59 @@
+
+import sys
+def minKey(key, mstSet,n): 
+    minn = sys.maxsize
+    for v in range(n): 
+        if key[v] < minn and mstSet[v] == False: 
+            minn = key[v] 
+            min_index = v 
+    return min_index 
+
+
+def prims(n, matrix):
+    key= [sys.maxsize]*n
+    #print(key)
+    parent = [None]*n
+    key[0]=0
+    mstSet = [False]*n
+    parent[0]=-1
+
+    for i in range(n):
+        u = minKey(key,mstSet,n)
+        mstSet[u]=True
+        print('%',u)
+        for v in range(n):
+            if matrix[u][v]>0 and mstSet==False and key[v]>matrix[u][v]:
+                key[v]=matrix[u][v]
+                parent[v]=u
+
+    #print(parent)
+
+
+nm = input().split()
+
+n = int(nm[0])
+
+m = int(nm[1])
+
+#edges = []
+
+matrix=[]
+for i in range(n):
+    matrix.append([0]*n)
+
+for _ in range(m):
+    a,b,weight= (list(map(int, input().rstrip().split())))
+    a-=1
+    b-=1
+    if matrix[a][b]!=0:
+        matrix[a][b]=min(matrix[a][b],weight)
+        matrix[b][a]=min(matrix[b][a],weight)
+    else:
+        matrix[a][b]=weight
+        matrix[b][a]=weight
+
+start = int(input())
+start-=1
+result = prims(n, matrix)
+
+print(result)
